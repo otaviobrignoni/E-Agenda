136824a9c97bbc228eb99fa28667c5fcb543a813
otaviobrignoni <otavio.678.b@gmail.com>
2025-06-17 14:08:20 -0300

    feat: Implementado repositório de compromisso

---
+++ E-Agenda.Infraestrutura/ModuloCompromissos/RepositorioCompromisso.cs
@@ -0,0 +1,12 @@
+﻿using E_Agenda.Dominio.ModuloCompromissos;
+using E_Agenda.Infraestrutura.Compartilhado;
+
+namespace E_Agenda.Infraestrutura.ModuloCompromissos;
+public class RepositorioCompromisso : RepositorioBase<Compromisso>, IRepositorioCompromisso
+{
+    public RepositorioCompromisso(ContextoDados contexto) : base(contexto) { }
+    protected override List<Compromisso> ObterRegistros()
+    {
+        return Contexto.Compromissos;
+    }
+}
