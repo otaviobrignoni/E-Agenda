88697a6635ef3edf0824ac263810ade7c1c11f92
AlexAraldi <alexanderaraldi@gmail.com>
2025-06-17 14:51:59 -0300

    feat: implementado IRepositorioContato

--- E-Agenda.Dominio/ModuloContatos/IRepositorioContato.cs
+++ E-Agenda.Dominio/ModuloContatos/IRepositorioContato.cs
@@ -1,4 +1,7 @@
-﻿namespace E_Agenda.Dominio.ModuloContatos;
-public interface IRepositorioContato
+﻿using E_Agenda.Dominio.Compartilhado;
+
+namespace E_Agenda.Dominio.ModuloContatos;
+public interface IRepositorioContato : IRepositorio<Contato>
 {
+
 }
