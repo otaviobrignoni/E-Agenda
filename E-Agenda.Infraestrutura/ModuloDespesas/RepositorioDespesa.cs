4e9921d8ff49a748001b8722e54e26e0331f2660
otaviobrignoni <otavio.678.b@gmail.com>
2025-06-18 16:38:44 -0300

    feat: Repositorio despesas

---
+++ E-Agenda.Infraestrutura/ModuloDespesas/RepositorioDespesa.cs
@@ -0,0 +1,14 @@
+﻿using E_Agenda.Dominio.ModuloDespesas;
+using E_Agenda.Infraestrutura.Compartilhado;
+
+
+namespace E_Agenda.Infraestrutura.ModuloDespesas;
+public class RepositorioDespesa : RepositorioBase<Despesa>, IRepositorioDespesa
+{
+    public RepositorioDespesa(ContextoDados contexto) : base(contexto) { }
+
+    protected override List<Despesa> ObterRegistros()
+    {
+        return Contexto.Despesas;
+    }
+}
