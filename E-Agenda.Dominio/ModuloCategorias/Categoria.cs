f300afc2f789096bb9af69b5ff142e6e9cad21ef
otaviobrignoni <otavio.678.b@gmail.com>
2025-06-18 14:29:35 -0300

    feat: Implementado entidade categoria;

---
+++ E-Agenda.Dominio/ModuloCategorias/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using E_Agenda.Dominio.Compartilhado;
+using E_Agenda.Dominio.ModuloDespesas;
+
+namespace E_Agenda.Dominio.ModuloCategorias;
+public class Categoria : EntidadeBase<Categoria>
+{
+    public string Titulo { get; set; }
+    public List<Despesa> Despesas { get; set; }
+
+    public override void Atualizar(Categoria registroEditado)
+    {
+        Titulo = registroEditado.Titulo;
+    }
+
+    public void AdicionarDespesa(Despesa despesa)
+    {
+        Despesas.Add(despesa);
+    }
+
+    public void RemoverDespesa(Despesa despesa)
+    {
+        Despesas.Remove(despesa);
+    }
+}
---
+++ E-Agenda.Dominio/ModuloDespesas/Despesa.cs
@@ -0,0 +1,10 @@
+﻿using E_Agenda.Dominio.Compartilhado;
+
+namespace E_Agenda.Dominio.ModuloDespesas;
+public class Despesa : EntidadeBase<Despesa>
+{
+    public override void Atualizar(Despesa registroEditado)
+    {
+        throw new NotImplementedException();
+    }
+}
