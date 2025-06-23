494610d8082bd68d9dfbea313452700a7f18bc41
Agatha Sates <satesagatha@gmail.com>
2025-06-23 05:11:51 -0300

    feat: Implementado Extensions de Tarefa

---
+++ E-Agenda.WebApp/Extensions/TarefaExtensions.cs
@@ -0,0 +1,26 @@
+﻿using E_Agenda.Dominio.ModuloTarefa;
+using E_Agenda.WebApp.Models;
+
+namespace E_Agenda.WebApp.Extensions;
+
+public static class TarefaExtensions
+{
+    public static Tarefa ParaEntidade(this FormTarefaViewModel formVM) 
+    {
+        return new Tarefa(formVM.Titulo, formVM.Prioridade);
+    }
+
+    public static DetalhesTarefaViewModel DetalhesVM(this Tarefa tarefa) 
+    {
+        return new DetalhesTarefaViewModel(
+            tarefa.Id,
+            tarefa.Titulo,
+            tarefa.NivelPrioridade,
+            tarefa.DataCriacao,
+            tarefa.DataConclusao,
+            tarefa.StatusTarefa,
+            tarefa.PercentualConcluido,
+            tarefa.Itens
+        );
+    }
+}
